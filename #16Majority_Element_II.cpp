7371d8e524929a85bc26f23ef98e6fee30beef04
Arpit Rajput <59957059+Arpit6424@users.noreply.github.com>
2023-06-13 19:00:01 +0530

    Create #16Majority_Element_II.cpp

---
+++ #16Majority_Element_II.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<int> ans;
+        
+        int candidate1 = INT_MIN;
+        int candidate2 = INT_MIN;
+        int count1 = 0;
+        int count2 =0;
+        
+        for(int i=0;i<n;i++){
+            int element = nums[i];
+            
+            if(element==candidate1)count1++;
+                
+            else if(element== candidate2)count2++;
+            
+            else if(count1==0) candidate1 = element , count1 = 1 ;
+            
+            else if(count2 ==0)candidate2 = element , count2 = 1;
+            
+            else
+                count1--,count2--;
+             
+            
+        }
+        
+        
+        count1 = 0, count2 =0;
+        
+        for(int i=0;i<n;i++){
+            int element = nums[i];
+            
+            if(element==candidate1)count1++;
+            if(element==candidate2)count2++;
+        }
+        
+        
+        if(count1>(n/3))ans.push_back(candidate1);
+        if(count2>(n/3))ans.push_back(candidate2);
+        
+        return ans;
+    
+    }
+       
+        
+
+};
