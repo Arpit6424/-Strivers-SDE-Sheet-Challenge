c1f70a14474796cb2c407fb057df0be6f2bdbee2
Arpit Rajput <59957059+Arpit6424@users.noreply.github.com>
2023-06-21 15:08:48 +0530

    Create Palindrome Linked List.cpp

---
+++ Day6/Palindrome Linked List.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+    
+    bool check(ListNode* &head1,ListNode* head2){
+        if(!head2)return true;
+           
+        
+        bool isP = check(head1,head2->next);        
+        if(head1->val != head2->val  )return false;        
+        head1 = head1->next;
+        return isP;
+    }   
+    
+public:
+    bool isPalindrome(ListNode* head) {
+        return check(head,head->next);
+
+        
+    }
+};
