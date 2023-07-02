329881741ffacf4052d2f2f0722e5fb3b4fa3be3
Arpit Rajput <59957059+Arpit6424@users.noreply.github.com>
2023-07-02 23:02:48 +0530

    Create Print all permutations of a string.cpp

---
+++ Day10/Print all permutations of a string.cpp
@@ -0,0 +1,34 @@
+class Solution {
+    
+    void solve(int ind,vector<int> &nums,vector<vector<int>> &res){
+        
+        if(ind == nums.size()){
+            res.push_back(nums);
+            return ;
+        }
+        
+        
+        for(int i=ind;i<nums.size();i++){
+            
+            swap(nums[ind],nums[i]);
+            solve(ind+1,nums,res);
+            swap(nums[ind],nums[i]);
+            
+        }
+        
+        
+        
+
+    }
+    
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+   
+        vector<vector<int>> res;
+  
+        solve(0,nums,res);
+        
+        return res;
+        
+    }
+};
